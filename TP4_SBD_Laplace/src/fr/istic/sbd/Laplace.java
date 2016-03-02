846ae48a58c054c958ae398be07c09c3199f2d4c
frochard <user1@148.60.14.89>
2016-03-02 12:22:56 +0100

    Question 6 en cours

---
+++ TP4_SBD_Laplace/src/fr/istic/sbd/Laplace.java
@@ -0,0 +1,38 @@
+package fr.istic.sbd;
+
+public class Laplace {
+
+	private double epsilon;
+	private double epsilonTotal;
+	private boolean test;
+	
+	public Laplace(double epsilon) {
+		this.epsilon = epsilon;
+		this.epsilonTotal=1;
+		this.test=true;
+	}
+
+
+	public Laplace() {
+
+	}
+
+	public double genNoise(int sensibilite, double proportionBudget){
+		double varAleatoire=0;
+		double consommable = this.epsilon+proportionBudget;
+		if(test || consommable<=this.epsilonTotal){
+			//Verification du mode
+			if(!test){
+				this.epsilon+=proportionBudget;
+				
+			}
+			//On definit une varaible u, tirée selon une loi uniforme continue dans l'intervalle [-1/2, 1/2], 
+			double u=Math.random()-0.5;
+			//Definition de la variable aleatoire
+			varAleatoire=0-sensibilite/proportionBudget*Math.signum(u)*Math.log(1-2*Math.abs(u));
+			
+		}
+		return varAleatoire;
+	}
+	
+}
--- TP4_SBD_Laplace/src/fr/istic/sbd/LaunchMe.java
+++ TP4_SBD_Laplace/src/fr/istic/sbd/LaunchMe.java
@@ -1,14 +1,58 @@
 package fr.istic.sbd;
 
-public class LaunchMe {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class LaunchMe {
+	
 	public LaunchMe() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Taille n du tableau d'entrée
+		int n=1000;
+		//Valeur maximale m de chaque elt du tableau
+		int m=20;
+		//Valeur parametre de confidentialite
+		double epsilon=0.0001;
+		// Generation du tableau
+		int[] tableau=new int[n];
+		int sensibilite=0;
+		for(int i=0;i<n;i++){
+			tableau[i]=(int) Math.round((Math.random()*m));
+			//On compte les valeurs inferieurs à m/2
+			if(tableau[i]>=m/2){
+				sensibilite++;
+			}
+		}
+		System.out.println(Arrays.toString(tableau));
+		
+		Laplace laplace = new Laplace(epsilon);
+		System.out.println("perturbation générée : "+ sensibilite);
+		double valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
+		valPerturbee=sensibilite+laplace.genNoise(sensibilite, 1);
+		System.out.println(valPerturbee);
 	}
-
 }
