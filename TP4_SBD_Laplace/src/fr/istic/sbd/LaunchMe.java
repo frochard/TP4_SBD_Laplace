017f004a4f8798138f5be592d87fa756c4bb5efc
frochard <user1@148.60.14.89>
2016-03-02 10:33:39 +0100

    1st commit

---
+++ TP4_SBD_Laplace/src/fr/istic/sbd/LaunchMe.java
@@ -0,0 +1,14 @@
+package fr.istic.sbd;
+
+public class LaunchMe {
+
+	public LaunchMe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
